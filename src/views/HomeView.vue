8ff5f985229cf36ee384a0cb5cf3a628131e9b53
Afiq Sazlan <afiqsazlan@gmail.com>
2023-06-19 17:03:29 +0800

    Implemented search country by region

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,12 +6,23 @@ import debounce from 'lodash.debounce'
 let isFetching = ref(true);
 let countries = ref([]);
 const search = ref('');
+const selectedRegion = ref('');
+
+const regions = [
+  'Asia',
+  'Oceania',
+  'Americas',
+  'Europe',
+  'Antarctic',
+  'Africa',
+]
 
 onMounted(async () => {
   const countriesFetched = await fetchCountries()
   // const countriesFetched = await fetchCountries()
   if (countriesFetched.success) {
     countries.value = countriesFetched.data
+    console.log(countries.value)
   } else {
     console.log(countriesFetched)
   }
@@ -22,8 +33,21 @@ watch(search, debounce(async (search) => {
   let countriesFetched = [];
   if (search) {
     countriesFetched = await searchCountriesByName(search)
+  } else {
+    countriesFetched = await fetchCountries()
+  }
 
+  if (countriesFetched.success) {
+    countries.value = countriesFetched.data
+  } else {
+    countries.value = []
+  }
+}, 500))
 
+watch(selectedRegion, debounce(async (region) => {
+  let countriesFetched = [];
+  if (region) {
+    countriesFetched = await searchCountriesByRegion(region)
   } else {
     countriesFetched = await fetchCountries()
   }
@@ -44,7 +68,7 @@ async function fetchCountries() {
 
   try {
     setIsFetching()
-    const response = await axios.get("https://restcountries.com/v3.1/all")
+    const response = await axios.get("https://restcountries.com/v3.1/all?fields=name,population,region,capital,flags")
     console.log(response.data[0])
     setIsFetching(false);
 
@@ -66,7 +90,7 @@ async function searchCountriesByName(name) {
   try {
     setIsFetching()
 
-    const response = await axios.get(`https://restcountries.com/v3.1/name/${name}`)
+    const response = await axios.get(`https://restcountries.com/v3.1/name/${name}?fields=name,population,region,capital,flags`)
     console.log(response.data[0])
     setIsFetching(false);
 
@@ -85,6 +109,37 @@ async function searchCountriesByName(name) {
   }
 }
 
+async function searchCountriesByRegion(region) {
+  try {
+    setIsFetching()
+
+    const response = await axios.get(`https://restcountries.com/v3.1/region/${region}?fields=name,population,region,capital,flags`)
+    console.log(response.data[0])
+    setIsFetching(false);
+
+    return {
+      success: true,
+      data: response.data
+    }
+  } catch (e) {
+    console.log(e)
+    setIsFetching(false);
+
+    return {
+      success: false,
+      errors: e
+    }
+  }
+}
+
+function resetSelectedRegion() {
+  selectedRegion.value = ''
+}
+
+function resetSearch() {
+  search.value = ''
+}
+
 function setIsFetching(value = true) {
   isFetching.value = value;
 }
@@ -97,9 +152,17 @@ function setIsFetching(value = true) {
       <div class="flex flex-row justify-between">
         <input v-model="search"
                placeholder="Search for a country..."
+               class="bg-blue-700 px-4 py-2 rounded-md"
         />
-        <select>
-          <option>Filter by Region</option>
+        <select v-model="selectedRegion" class="bg-blue-700 px-4 py-2 rounded-md"
+
+        >
+          <option :value="null">Filter by Region</option>
+          <option v-for="region in regions"
+                  :key="`region-${region}`"
+          >
+            {{ region }}
+          </option>
         </select>
       </div>
 
