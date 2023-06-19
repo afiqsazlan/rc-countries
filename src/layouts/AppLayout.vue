ea06d5f4e7247ca7080b180139a642ab24c36f6d
Afiq Sazlan <afiqsazlan@gmail.com>
2023-06-19 16:14:34 +0800

    Implemented app layout

---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class=" bg-blue-900 min-h-screen text-white">
+    <div class="flex justify-between  px-6 py-4">
+      <div>
+        Where in the world?
+      </div>
+      <button>
+        Dark Mode
+      </button>
+    </div>
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./index.html",
+        "./src/**/*.{vue,js,ts,jsx,tsx}",
+    ],
+    theme: {
+        colors: {
+            'blue-700': 'hsl(209, 23%, 22%)',
+            'blue-800': 'hsl(207, 26%, 17%)',
+            'blue-900': 'hsl(200, 15%, 8%)',
+            'gray-700': 'hsl(0, 0%, 52%)',
+            'gray-100': 'hsl(0, 0%, 98%)',
+            'white': 'hsl(0, 0%, 100%)'
+        },
+        extend: {
+          fontFamily: {
+            'sans': ['Nunito Sans', ...defaultTheme.fontFamily.sans],
+          },
+        },
+    },
+    plugins: [],
 }
 
