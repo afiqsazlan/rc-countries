2b374d4598c1fb6335d57a6c7cd05a4db4cba9cf
Afiq Sazlan <afiqsazlan@gmail.com>
2023-06-20 17:19:05 +0800

    Removed typescript

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import './assets/style.css'
 
-import {createApp} from 'vue'
-import {createPinia} from 'pinia'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
 import App from './App.vue'
 import router from './router'
 
-/* import the fontawesome core */
+
 import {library} from '@fortawesome/fontawesome-svg-core'
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 import {faMoon, faSun} from '@fortawesome/free-regular-svg-icons'
@@ -13,12 +14,9 @@ import {faArrowLeft, faMagnifyingGlass, faPersonRunning, faPersonCircleExclamati
 
 library.add(faMoon, faSun, faArrowLeft, faMagnifyingGlass, faPersonRunning, faPersonCircleExclamation)
 
-const pinia = createPinia()
 const app = createApp(App)
 
-app.component('font-awesome-icon', FontAwesomeIcon)
-
+app.use(createPinia())
 app.use(router)
-app.use(pinia)
 
 app.mount('#app')
