d3939aa73c9f198ca6d3eb2b51ac12cec08bbd01
Afiq Sazlan <afiqsazlan@gmail.com>
2023-06-20 14:06:26 +0800

    Added padding and changed text for not found and fetching data screen

--- src/components/FetchingDataScreen.vue
+++ src/components/FetchingDataScreen.vue
@@ -2,14 +2,14 @@
 </script>
 
 <template>
-  <div class="flex flex-col space-y-4 items-center min-h-[50vh] justify-center">
+  <div class="flex flex-col space-y-4 items-center px-8 text-center min-h-[50vh] justify-center">
     <font-awesome-icon :icon="['fas', 'person-running']"
                        class="text-blue-900 dark:text-white"
                        bounce
                        size="2xl"
     />
     <p class="text-sm">
-      Let us fetch something good for you.
+      Fetching some data. Hang tight, we're on it!
     </p>
   </div>
 </template>
--- src/components/NotFoundScreen.vue
+++ src/components/NotFoundScreen.vue
@@ -3,14 +3,14 @@
 </script>
 
 <template>
-  <div class="flex flex-col space-y-4 items-center min-h-[50vh] justify-center">
+  <div class="flex flex-col space-y-4 items-center min-h-[50vh] px-8 text-center justify-center">
     <font-awesome-icon :icon="['fas', 'person-circle-exclamation']"
                        class="text-blue-900 dark:text-white"
                        shake
                        size="2xl"
     />
-    <p class="text-sm">
-      Oops! We couldn't find any countries for you.
+    <p class="text-sm font-bold">
+      Oops! Looks like the resource you're looking for couldn't be found.
     </p>
   </div>
 </template>
